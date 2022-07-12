673602b5ef412b18225f5902006e06eab845ac3f
Shending-Help <yusufcheetos99@gmail.com>
2022-07-12 15:11:30 +0200

    added type defenitions

--- src/routes/api/processing.ts
+++ src/routes/api/processing.ts
@@ -16,8 +16,7 @@ processing.get('/', (req, res) => {
     sharp(inputFile)
     .resize(300, 200)
     .toFile(outputFile, function(err) {
-      // output.jpg is a 300 pixels wide and 200 pixels high image
-      // containing a scaled and cropped version of input.jpg
+      
     });    
     
 
