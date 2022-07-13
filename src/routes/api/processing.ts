063dff78f529e466da874a8185115d56cb50b3d1
Shending-Help <yusufcheetos99@gmail.com>
2022-07-13 06:45:49 +0200

    cached requests

--- src/routes/api/processing.ts
+++ src/routes/api/processing.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import sharp from "sharp";
 import path from "path";
+const fs = require("fs");
 //import { promises as fsPromises } from "fs";
 
 const processing = express.Router();
@@ -10,26 +11,28 @@ const processing = express.Router();
 
 processing.get("/", (req, res) => {
   const inputFile = "images/" + req.query.filename;
-  const outputFile = inputFile.substring(7);
+  // const outputFile = inputFile.substring(7);
   const width = Number(req.query.width);
   const height = Number(req.query.height);
-
-  sharp(inputFile)
-    .resize((width as unknown) as number, (height as unknown) as number, {
-      fit: "fill",
-    })
-    .toFile(`output/${width}_${height}_` + outputFile, function(err) {})
-    .toBuffer()
-    .then((data) => {
-      res.sendFile(
-        path.normalize(
-          __dirname + `../../../../output/${width}_${height}_` + outputFile
-        )
-      );
-    })
-    .catch((err) => {
-      res.send("image couldn't be retreived: " + err);
-    });
+  const outputFile = `output/${width}_${height}_` + req.query.filename;
+  fs.access(outputFile, (err: any) => {
+    if (err) {
+      sharp(inputFile)
+        .resize((width as unknown) as number, (height as unknown) as number, {
+          fit: "fill",
+        })
+        .toFile(outputFile, function(err) {})
+        .toBuffer()
+        .then((data) => {
+          res.sendFile(path.normalize(__dirname + `../../../../` + outputFile));
+        })
+        .catch((err) => {
+          res.send("image couldn't be retreived: " + err);
+        });
+    } else {
+      res.sendFile(path.normalize(__dirname + `../../../../` + outputFile));
+    }
+  });
 });
 
 export default processing;
